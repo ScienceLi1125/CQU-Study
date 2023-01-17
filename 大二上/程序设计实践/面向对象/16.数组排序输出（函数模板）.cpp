984fb47379367b36fec039fa780f1df608948e57
ScienceLi1125 <2075701634@qq.com>
2023-01-17 16:23:34 +0800

    增加大二上学习资料

--- 大二上/程序设计实践/面向对象/16.数组排序输出（函数模板）.cpp
+++ 大二上/程序设计实践/面向对象/16.数组排序输出（函数模板）.cpp
@@ -3,18 +3,18 @@
 #include <algorithm>
 using namespace std;
 
-template <class T>
-void sort(T* a,int n){      //输入的第一个形参为T型指针，指代数组
-    for(int i=0;i<n;i++){   
-        cin>>a[i];          //函数内部也支持输入
-    }
-    sort(a,a+n);            //调用algorithm中sort函数
+template <typename T>		//普通函数模板 
+void sortT(T* arr,int n){   	//输入的第一个形参为T型指针，指代数组
+    for(int i=0;i<n;i++){
+    	cin>>arr[i];
+	}
+    sort(arr,arr+n);
 }
 
 template <class T>
-void display(T* a, int size){
-    for(int i=0; i<size-1; i++) cout<<a[i]<<' ';
-    cout<<a[size-1]<<endl;
+void display(T* arr, int size){
+    for(int i=0; i<size-1; i++) cout<<arr[i]<<' ';
+    cout<<arr[size-1]<<endl;
 }
 
 int main() {
@@ -27,11 +27,16 @@ int main() {
     cin>>ty;
     while(ty>0){
         cin>>size;
+        if(size>SIZE){
+        	cout<<"Too large array!"<<endl;
+        	exit(-1);
+		}
         switch(ty){
-            case 1:sort(a,size); display(a,size); break;
-            case 2:sort(b,size); display(b,size); break;
-            case 3:sort(c,size); display(c,size); break;
-            case 4:sort(d,size); display(d,size); break;
+            case 1:sortT<int>(a,size); display(a,size); break;
+            case 2:sortT<char>(b,size); display(b,size); break;
+            case 3:sortT<double>(c,size); display(c,size); break;
+            case 4:sortT<string>(d,size); display(d,size); break;
+            default:cout<<"Wrong Input!"<<endl; break;
         }
         cin>>ty;
     }
